875e7288e3f0d19671cfe9145006516d317253a0
kiruba <kirub@Shankar>
2022-08-10 23:43:29 +0530

    Commit

---
+++ Phase2/src/LogIn/DashBoard.java
@@ -0,0 +1,35 @@
+package LogIn;
+
+import java.io.*;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/dashBoard")
+public class DashBoard extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+        
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                
+        	PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            
+            HttpSession session=request.getSession(false);  
+            String userId = null;
+            if (session.getAttribute("userid") != null)
+                userId =(String)session.getAttribute("userid");  
+            if (userId == null ) {
+                out.println("No UserId was found in session.<br>");
+            } else {
+                out.println("UserId obtained from session :" + userId + "<br>");
+                out.println("<a href='logout'>Logout of session</a><br>");
+            }
+            out.println("</body></html>");
+        }
+
+}
